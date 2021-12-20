8ed7119dd9c6e3d3fdf133aa5f488b3126a12ac5
Rhio Sutoyo <rhio.sutoyo@gmail.com>
2021-12-20 16:00:04 +0700

    TSP

---
+++ Travelling Salesman Problem/tsp_backtracking.cpp
@@ -0,0 +1,86 @@
+/*source: 
+https://www.geeksforgeeks.org/travelling-salesman-problem-implementation-using-backtracking/?ref=lbp
+*/
+
+// C++ implementation of the approach
+
+#include <bits/stdc++.h>
+using namespace std;
+#define V 4
+
+// Function to find the minimum weight Hamiltonian Cycle
+void tsp(int graph[][V], vector<bool>& v, int currPos,
+		int n, int count, int cost, int& ans)
+{
+
+	// If last node is reached and it has a link
+	// to the starting node i.e the source then
+	// keep the minimum value out of the total cost
+	// of traversal and "ans"
+	// Finally return to check for more possible values
+	if (count == n && graph[currPos][0]) {
+		ans = min(ans, cost + graph[currPos][0]);
+		return;
+	}
+
+	// BACKTRACKING STEP
+	// Loop to traverse the adjacency list
+	// of currPos node and increasing the count
+	// by 1 and cost by graph[currPos][i] value
+	for (int i = 0; i < n; i++) {
+		if (!v[i] && graph[currPos][i]) {
+
+			// Mark as visited
+			v[i] = true;
+			tsp(graph, v, i, n, count + 1,
+				cost + graph[currPos][i], ans);
+
+			// Mark ith node as unvisited
+			v[i] = false;
+		}
+	}
+};
+
+// Driver code
+int main()
+{
+	// n is the number of nodes i.e. V
+	int n = 4;
+
+	int graph[][V] = {
+		{ 0, 10, 15, 20 },
+		{ 10, 0, 35, 25 },
+		{ 15, 35, 0, 30 },
+		{ 20, 25, 30, 0 }
+	};
+	
+	// example graph from slide "COMP6049 Pert 15-16"
+	int graph_2[][V] = {
+		{ 0, 12, 11, 16},
+		{15,  0, 15, 10},
+		{ 8, 14,  0, 18},
+		{ 9, 11, 17,  0}
+	};
+	
+	// Boolean array to check if a node
+	// has been visited or not
+	vector<bool> v(n);
+	for (int i = 0; i < n; i++)
+		v[i] = false;
+
+	// Mark 0th node as visited
+	v[0] = true;
+	int ans = INT_MAX;
+
+	// Find the minimum weight Hamiltonian Cycle
+	// sent graph, visit flag, curr_position, number of nodes, visiting count, total cost, and answer (for return value)
+
+//	tsp(graph, v, 0, n, 1, 0, ans);
+	tsp(graph_2, v, 0, n, 1, 0, ans);
+
+	// ans is the minimum weight Hamiltonian Cycle
+	cout << "Shortest Path: " << ans;
+
+	return 0;
+}
+
